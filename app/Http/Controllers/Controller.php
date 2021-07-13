53e24ac6818e23cf674da66c6bb645f88cb4ea8a
Abubakar Lawal <abula3003@gmail.com>
2021-07-13 18:50:41 +0100

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -25,33 +25,5 @@ class Controller extends BaseController
 
     public function test()
     {
-
-        $message = Message::find(2);
-        $res = Http::withHeaders(
-            [
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer AAAAtdlHoJk:APA91bFiPCLhv9J7CPfuT70LAnGJKnj4qhRYZ3sKUmgasERT2klh-hPiiHnC3xVxeQ8BdU5R-GVGd-6bW2uBaXL0piJWFirjSHkqDw1JWjChncBMC1dZ3I_D5ChbZ8RRwb_gUM080utA'
-            ]
-        )->post('https://fcm.googleapis.com/fcm/send', [
-            //"to" => "/topics/messaging",
-            "registration_ids" => ["eDH7leOaQPenBvUqWGHen6:APA91bFLux82IiyPpRsE5q5Fc4FKLW6kcL222znz4ldIHAyuOTbKsrEdJArYpzZzDLtXlPQv2crXlnNQXg3H2BJm2AeiJBHdPrZA7WH8d1cFa5tnPM0zAJzTWAnVZTyNtpX1wGMnBDrk"],
-            /* "notification" => [
-                "title" =>$message->sen,
-                "body" => "messaging tutorial"
-            ], */
-            "data" => new MessageResource($message)
-        ])->throw();
-
-        return $res->json();
-        $user = User::find(1);
-        //return Sms::sendSms('8106813749', 'test');
-        return $user->image_url;
-        Auth::login(User::find(2));
-
-        broadcast(new UserOnline(User::find(2)))->toOthers();
-        return;
-        broadcast(new ChatLoaded(new ChatResource(Chat::find(request()->id ?? 1)->load(['messages', 'users']))))->toOthers();
-        //broadcast(new MessageCreated(new MessageResource(Chat::find(1)->messages->first())))->toOthers();
-
     }
 }
